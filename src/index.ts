fcf34a2fe2e83336744109dc2b6b5150bde4b1e1
Magicoo51889 <jackot11f@gmail.com>
2021-08-26 21:15:06 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Client } from 'discord.js';
 import { model, Schema, Document, connect } from 'mongoose';
 import "dotenv/config"
 
-connect("mongodb+srv://Magicoo51889:<password>@cluster0.7xetp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" , {
+connect("mongodb+srv://Magicoo51889:<1tgw6K1CquZC5Cj3>@cluster0.7xetp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("Ego is connected to mongodb"));
